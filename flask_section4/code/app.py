af1ac4de8aaf7295eaf1d6e76d8e1ceaba3dffef
Nick Schmidt <nick@schmidtyanimation.com>
2018-07-14 07:10:37 -0500

    adding get and post functionalities and cleaning up code with validation

--- flask_section4/code/app.py
+++ flask_section4/code/app.py
@@ -1,14 +1,31 @@
-from flask import flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'nick'
 api = Api(app)
 
+items = []
+
 # api works with resources, every resource needs to be a class:
-class Student(Resource):
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
-api.add_resource(Student, '/student/<string:name>')
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.  ".format(name)}, 400
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201 # 201 is created status
 
-app.run(port=8080)
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=8080, debug=True)
